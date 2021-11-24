b8775effecebcfaabb02839a7415f73fc410814f
Nattakit Samreth <tannyishappy@gmail.com>
2021-11-25 03:44:26 +0700

    first commit

---
+++ subquery-account-balance-main/src/mappings/mappingHandlers.ts
@@ -0,0 +1,17 @@
+import { SubstrateEvent } from "@subql/types";
+import { Account } from "../types";
+import { Balance } from "@polkadot/types/interfaces";
+
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+    const { event: { data: [account, balance] } } = event;
+    let record = new Account(event.extrinsic.block.block.header.hash.toString());
+    //Retrieve the record by its ID
+    record.account = account.toString();
+    //Big integer type Balance of a transfer event
+    record.balance = (balance as Balance).toBigInt();
+    await record.save();
+}
+
+
+
